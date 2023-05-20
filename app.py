8eebfbcbedf414d93e27509f31e942d4ab16a1b6
Willythepo0h <133613298+Willythepo0h@users.noreply.github.com>
2023-05-20 21:01:28 +0800

    app.py

--- app.py
+++ app.py
@@ -27,6 +27,6 @@ else:
     image=Image.open(file)
     st.image(image,use_column_width=True)
     prediction=import_and_predict(image,model)
-    class_names=['Shine',,'Rain',]
+    class_names=['Shine','Rain',]
     string="OUTPUT : "+class_names[np.argmax(prediction)]
     st.success(string)
