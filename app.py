22f35d4f57009ab2ed787cd2ad0e29074ef924f1
Willythepo0h <133613298+Willythepo0h@users.noreply.github.com>
2023-10-12 23:05:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ file=st.file_uploader("Choose a photo from computer",type=["jpg","png"])
 
 def import_and_predict(image_data,model):
     size=(224,224)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
+    image=ImageOps.fit(image_data,size,Image.LANCZOS)
     img=np.asarray(image)
     img_reshape=img[np.newaxis,...]
     prediction=model.predict(img_reshape)
