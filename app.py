0a678d4a3667dc97e9553f42f92943d48cbc70c2
Willythepo0h <133613298+Willythepo0h@users.noreply.github.com>
2023-10-12 22:53:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import tensorflow as tf
-from PIL import Image
+from PIL import Image, ImageOps
+import numpy as np
 
 @st.cache(allow_output_mutation=True)
 def load_model():
   model=tf.keras.models.load_model('model_cnn.hdf5')
   return model
+  
 model=load_model()
 st.write("""
 # Weather Classification Model"""
@@ -21,9 +23,6 @@ with st.container():
     
 file=st.file_uploader("Choose a photo from computer",type=["jpg","png"])
 
-import cv2
-from PIL import Image,ImageOps
-import numpy as np
 def import_and_predict(image_data,model):
     size=(224,224)
     image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
@@ -31,6 +30,7 @@ def import_and_predict(image_data,model):
     img_reshape=img[np.newaxis,...]
     prediction=model.predict(img_reshape)
     return prediction
+  
 if file is None:
     st.text("Please upload an image file")
 else:
