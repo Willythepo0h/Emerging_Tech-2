345e1e385d13667d380e78f7d50a43df4b50c726
Willythepo0h <133613298+Willythepo0h@users.noreply.github.com>
2023-10-16 01:46:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,22 @@ import numpy as np
 def load_model():
   model=tf.keras.models.load_model('model_cnn.hdf5')
   return model
-  
+
+def test_load_model():
+    model = load_model()
+    assert isinstance(model, tf.keras.models.Model)
+
+# Define a test function for the import_and_predict function
+def test_import_and_predict():
+    # Mock image data for testing
+    image_data = Image.new('RGB', (224, 224))
+    model = load_model()
+    num_classes = 2  # Replace with the actual number of classes in your model
+    prediction = import_and_predict(image_data, model, num_classes)
+    assert prediction.shape == (1, num_classes)
+
 model=load_model()
-st.write("""
-# Weather Classification Model"""
-        )
+st.write("""#Weather Classification Model""")
 
 image = Image.open('Weather_girl.jpg')
 st.image(image, caption='Weather Classification Model - John Willard S. Sucgang')
@@ -43,3 +54,5 @@ else:
     
 st.info("""Github Repository Link: https://github.com/Willythepo0h/Emerging_Tech-2""")
 st.info("""Google Colab Link: https://colab.research.google.com/drive/1z8Q1byGelG2QqQRY66CjqP1ky4lM3IL_?usp=sharing""")
+if __name__ == "__main__":
+    main()
