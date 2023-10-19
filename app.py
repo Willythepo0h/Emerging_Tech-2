9ed0bd3735008cee9edb3ad050fff605abf50c14
Willythepo0h <133613298+Willythepo0h@users.noreply.github.com>
2023-10-19 23:35:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ model = load_model()
 st.write("""
 # Weather Classification Model
 """)
-st.write("This web application classify weather conditions in uploaded images. Please follow the steps:")
+st.write("This web app classify weather conditions in uploaded images. Please follow the steps:")
 st.markdown("1. Upload an image using the 'Browse Files' button.")
 st.markdown("2. Wait for the model to process the image.")
 st.markdown("3. View the prediction and confidence score.")
